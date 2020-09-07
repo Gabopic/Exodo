7f912e36f93237f7c5d8473e4c8e25244613ca58
Gabopic <gabo.pichihueche@gmail.com>
2020-09-07 02:06:54 -0300

    Cambio1

--- Operaciones.py
+++ Operaciones.py
@@ -6,4 +6,11 @@ print (f'La suma es primero, por ejemplo, y+x={y}+{x}={y+x}')
 
 print (f'Luego va la resta, por ejemplo, y-x={y}-{x}={y-x}')
 
+print (f'Ahora la multiplicación, por ejemplo, y*x={y}*{x}={y*x}')
+
+print (f'Por último la división, por ejemplo, y/x={y}/{x}={y//x}')
+
+print (f'Y el resto de esta división es {y%x}')
+
+
 
