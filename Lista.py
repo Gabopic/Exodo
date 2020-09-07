54a929677f0821419cee1424bb3e1c8bee539be3
Gabopic <gabo.pichihueche@gmail.com>
2020-09-07 03:46:54 -0300

    Cambio2

--- Lista.py
+++ Lista.py
@@ -1,1 +1,18 @@
-Nombre
+
+diccionario = {'Nombres':['Lupita', 'Wanolo', 'Lucho', 'Cristobal'], 'Apellidos':['Uchiha', 'Kurosaki', 'Jeager', 'Sifón'], 'Notas':[[1.0,2.0], [6.5,6.0], [4.0,4.0], [7.0,7.0]], 'Cursos': ['Programación', 'Física1']}
+
+print (diccionario['Nombres'][0])
+print (diccionario['Apellidos'][0])
+
+print (diccionario['Notas'][0][0])
+print (diccionario['Cursos'][0])
+
+print (diccionario['Notas'][0][1])
+print (diccionario['Cursos'][1])
+
+
+
+
+
+
+
